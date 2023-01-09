2e54f1345839af881aec90b38e446ede0f20db97
gustavohmf2 <gustavo.hmf20@gmail.com>
2023-01-09 18:41:48 -0300

    start project

---
+++ src/main/java/com/youvotematter/votemanager/VotemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.youvotematter.votemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotemanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youvotematter/votemanager/VotemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youvotematter.votemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
